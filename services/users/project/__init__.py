2ef704a03bd7ae300f58371d16ab92ff6559a9c4
steve.orr <mr.steve.orr@gmail.com>
2019-01-14 15:54:54 -0700

    part2_ch4

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -21,6 +21,7 @@ def create_app(script_info=None):
     # register blueprints
     from project.api.users import users_blueprint
     app.register_blueprint(users_blueprint)
+
     # shell context for flask cli
     @app.shell_context_processor
     def ctx():
