0419f8becca839d029f547f44672903322203bee
steve.orr <mr.steve.orr@gmail.com>
2019-01-14 15:35:32 -0700

    part2_ch4

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -1,6 +1,4 @@
 # services/users/project/config.py
-
-
 import os
 
 
@@ -9,11 +7,14 @@ class BaseConfig:
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'my_precious'
+    DEBUG_TB_ENABLED = False
+    DEBUG_TB_INTERCEPT_REDIRECTS = False
 
 
 class DevelopmentConfig(BaseConfig):
     """Development configuration"""
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    DEBUG_TB_ENABLED = True
 
 
 class TestingConfig(BaseConfig):
